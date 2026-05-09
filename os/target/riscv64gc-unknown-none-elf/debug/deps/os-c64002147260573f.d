aa858cabb599b43703352e47df61f9e7588ce087
Yhr <23301142@bjtu.edu.cn>
2026-05-09 11:30:51 +0800

    ex1: feat: remove std and main by Haoran Yang(23301142) in 20260509

---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-c64002147260573f.d
@@ -0,0 +1,5 @@
+/home/HaoranYang/ExOS/os/target/riscv64gc-unknown-none-elf/debug/deps/os-c64002147260573f: src/main.rs
+
+/home/HaoranYang/ExOS/os/target/riscv64gc-unknown-none-elf/debug/deps/os-c64002147260573f.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/debug/os.d
@@ -0,0 +1,1 @@
+/home/HaoranYang/ExOS/os/target/riscv64gc-unknown-none-elf/debug/os: /home/HaoranYang/ExOS/os/src/main.rs
