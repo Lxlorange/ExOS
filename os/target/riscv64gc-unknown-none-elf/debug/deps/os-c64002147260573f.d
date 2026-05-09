b335a88e861a437f573c465e59f5cba24f4609d0
Yhr <23301142@bjtu.edu.cn>
2026-05-09 11:35:24 +0800

    fix: remove target directory from git tracking

--- os/target/riscv64gc-unknown-none-elf/debug/deps/os-c64002147260573f.d
+++
@@ -1,5 +0,0 @@
-/home/HaoranYang/ExOS/os/target/riscv64gc-unknown-none-elf/debug/deps/os-c64002147260573f: src/main.rs
-
-/home/HaoranYang/ExOS/os/target/riscv64gc-unknown-none-elf/debug/deps/os-c64002147260573f.d: src/main.rs
-
-src/main.rs:
--- os/target/riscv64gc-unknown-none-elf/debug/os.d
+++
@@ -1,1 +0,0 @@
-/home/HaoranYang/ExOS/os/target/riscv64gc-unknown-none-elf/debug/os: /home/HaoranYang/ExOS/os/src/main.rs
