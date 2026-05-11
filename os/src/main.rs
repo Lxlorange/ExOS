ad0beb57253fd3b92841fdabc1bce2ea76b11ec5
Yhr <23301142@bjtu.edu.cn>
2026-05-11 12:20:55 +0800

    ex2: feat: Establish a bare-metal execution environment and minimize the kernel By Haoran Yang in 20260511

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,69 +1,48 @@
 #![no_std]
 #![no_main]
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
 
-use core::arch::asm;
-use core::fmt::{self, Write};
-use core::panic::PanicInfo;
+use core::arch::global_asm;
 
-const SYSCALL_WRITE: usize = 64;
-const SYSCALL_EXIT: usize = 93;
-
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
-    unsafe {
-        asm!("ecall",
-             in("x10") args[0],
-             in("x11") args[1],
-             in("x12") args[2],
-             in("x17") id,
-             lateout("x10") ret
-        );
+global_asm!(include_str!("entry.asm"));
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
     }
-    ret
-}
-
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
-}
-
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
-}
-
-struct Stdout;
-
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        sys_write(1, s.as_bytes());
-        Ok(())
-    }
-}
-
-pub fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
-}
-
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::print(format_args!($fmt $(, $($arg)+)?));
-    }
-}
-
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    let sbss_ptr = sbss as *const () as usize;
+    let ebss_ptr = ebss as *const () as usize;
+    (sbss_ptr..ebss_ptr).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 
 #[no_mangle]
-extern "C" fn _start() {
-    println!("Hello, 2330142 world!");
-    sys_exit(9);
+pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
+    clear_bss();
+    println!("Hello, world!");
+    println!(".text [{:#x}, {:#x})", stext as *const () as usize, etext as *const () as usize);
+    println!(".rodata [{:#x}, {:#x})", srodata as *const () as usize, erodata as *const () as usize);
+    println!(".data [{:#x}, {:#x})", sdata as *const () as usize, edata as *const() as usize);
+    println!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as *const () as usize, boot_stack_top as *const () as usize
+    );
+    println!(".bss [{:#x}, {:#x})", sbss as *const () as usize, ebss as *const () as usize);
+    println!("Hello, 23301142 world!");
+    panic!("Shutdown machine!");
 }
+
