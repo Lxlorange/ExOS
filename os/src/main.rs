17d705f1d14a336b00b91b4e7e9d6fec8088e7b9
Yhr <23301142@bjtu.edu.cn>
2026-05-18 11:41:01 +0800

    ex3: feat: Batch processing and privilege levels by Haoran Yang(23301142) in 20260518

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,15 +1,21 @@
 #![no_std]
 #![no_main]
+
 #[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
+mod syscall;
+mod trap;
+mod batch;
 
 use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
 fn clear_bss() {
-    extern "C" {
+    unsafe extern "C" {
         fn sbss();
         fn ebss();
     }
@@ -18,31 +24,12 @@ fn clear_bss() {
     (sbss_ptr..ebss_ptr).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
-    extern "C" {
-        fn stext();
-        fn etext();
-        fn srodata();
-        fn erodata();
-        fn sdata();
-        fn edata();
-        fn sbss();
-        fn ebss();
-        fn boot_stack();
-        fn boot_stack_top();
-    }
     clear_bss();
-    println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as *const () as usize, etext as *const () as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as *const () as usize, erodata as *const () as usize);
-    println!(".data [{:#x}, {:#x})", sdata as *const () as usize, edata as *const() as usize);
-    println!(
-        "boot_stack [{:#x}, {:#x})",
-        boot_stack as *const () as usize, boot_stack_top as *const () as usize
-    );
-    println!(".bss [{:#x}, {:#x})", sbss as *const () as usize, ebss as *const () as usize);
-    println!("Hello, 23301142 world!");
-    panic!("Shutdown machine!");
+    println!("[Kernel] Hello, batch operating system!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
 
