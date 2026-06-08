885a7d07f68862f987643d1c969e2ad2dea513d9
Yhr <23301142@bjtu.edu.cn>
2026-06-08 11:03:40 +0800

    ex6: feat: step 2 by Haoran Yang(2330142) in 20260608

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,5 +1,7 @@
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
+pub const PAGE_SIZE: usize = 4096;
+pub const PAGE_SIZE_BITS: usize = 12;
 pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
 pub const MAX_APP_NUM: usize = 4;
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,4 +1,16 @@
+mod address;
 mod heap_allocator;
+
+pub use address::{
+    PhysAddr, PhysPageNum, SimpleRange, SimpleRangeIterator, StepByOne, VirtAddr, VirtPageNum,
+    PPNRange, VPNRange,
+};
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct PageTableEntry {
+    pub bits: usize,
+}
 
 pub fn init() {
     heap_allocator::init_heap();
