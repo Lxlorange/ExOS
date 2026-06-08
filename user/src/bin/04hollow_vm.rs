e2059c71e6c13be6c92427d34e23b5deff8c3192
Yhr <23301142@bjtu.edu.cn>
2026-06-08 12:13:00 +0800

    ex6: feat: app for question5 by Haoran Yang(2330142) in 20260608

---
+++ user/src/bin/04hollow_vm.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate user_lib;
+
+use user_lib::{exit, get_time, yield_};
+
+#[no_mangle]
+fn main() -> i32 {
+    println!("hello_vm: start");
+    for i in 0..5 {
+        println!("hello_vm: round {}, time = {}", i, get_time());
+        yield_();
+    }
+    println!("hello_vm: exit");
+    exit(0);
+    0
+}
