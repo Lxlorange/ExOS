2076ec23ea54afd29a0c52100e5616fede09002c
Yhr <23301142@bjtu.edu.cn>
2026-06-08 11:42:19 +0800

    ex6: feat: step 6 by Haoran Yang(2330142) in 20260608

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -1,10 +1,8 @@
-//os/src/loader.rs
-
 use core::arch::asm;
 
-use crate::trap::TrapContext;
-use crate::task::TaskContext;
 use crate::config::*;
+use crate::task::TaskContext;
+use crate::trap::TrapContext;
 
 #[repr(align(4096))]
 #[derive(Copy, Clone)]
@@ -54,42 +52,66 @@ fn get_base_i(app_id: usize) -> usize {
     APP_BASE_ADDRESS + app_id * APP_SIZE_LIMIT
 }
 
+fn get_app_entry(app_id: usize) -> usize {
+    let elf = xmas_elf::ElfFile::new(get_app_data(app_id)).unwrap();
+    elf.header.pt2.entry_point() as usize
+}
+
 pub fn get_num_app() -> usize {
     extern "C" { fn _num_app(); }
     unsafe { (_num_app as *const () as *const usize).read_volatile() }
 }
 
+pub fn get_app_data(app_id: usize) -> &'static [u8] {
+    extern "C" {
+        fn _num_app();
+    }
+    let num_app_ptr = _num_app as *const () as *const usize;
+    let num_app = get_num_app();
+    let app_start = unsafe { core::slice::from_raw_parts(num_app_ptr.add(1), num_app + 1) };
+    assert!(app_id < num_app);
+    unsafe {
+        core::slice::from_raw_parts(
+            app_start[app_id] as *const u8,
+            app_start[app_id + 1] - app_start[app_id],
+        )
+    }
+}
+
 pub fn init_app_cx(app_id: usize) -> &'static TaskContext {
     KERNEL_STACK[app_id].push_context(
-        TrapContext::app_init_context(get_base_i(app_id), USER_STACK[app_id].get_sp()),
+        TrapContext::app_init_context(
+            get_base_i(app_id) + get_app_entry(app_id),
+            USER_STACK[app_id].get_sp(),
+        ),
         TaskContext::goto_restore(),
     )
 }
 
 
 pub fn load_apps() {
-    extern "C" { fn _num_app(); }
-    let num_app_ptr = _num_app as *const () as *const usize;
     let num_app = get_num_app();
-    let app_start = unsafe {
-        core::slice::from_raw_parts(num_app_ptr.add(1), num_app + 1)
-    };
-    // clear i-cache first
-    unsafe { asm!("fence.i"); }
-    // load apps
     for i in 0..num_app {
         let base_i = get_base_i(i);
-        // clear region
         (base_i..base_i + APP_SIZE_LIMIT).for_each(|addr| unsafe {
             (addr as *mut u8).write_volatile(0)
         });
-        // load app from data section to memory
-        let src = unsafe {
-            core::slice::from_raw_parts(app_start[i] as *const u8, app_start[i + 1] - app_start[i])
-        };
-        let dst = unsafe {
-            core::slice::from_raw_parts_mut(base_i as *mut u8, src.len())
-        };
-        dst.copy_from_slice(src);
+        let elf_data = get_app_data(i);
+        let elf = xmas_elf::ElfFile::new(elf_data).unwrap();
+        let ph_count = elf.header.pt2.ph_count();
+        for j in 0..ph_count {
+            let ph = elf.program_header(j).unwrap();
+            if ph.get_type().unwrap() == xmas_elf::program::Type::Load {
+                let start = base_i + ph.virtual_addr() as usize;
+                let end = start + ph.mem_size() as usize;
+                assert!(end <= base_i + APP_SIZE_LIMIT);
+                let src = &elf.input[ph.offset() as usize..(ph.offset() + ph.file_size()) as usize];
+                let dst = unsafe { core::slice::from_raw_parts_mut(start as *mut u8, src.len()) };
+                dst.copy_from_slice(src);
+            }
+        }
+    }
+    unsafe {
+        asm!("fence.i");
     }
 }
